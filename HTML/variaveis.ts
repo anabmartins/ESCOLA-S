8847aa73e2f29b74e340673d55e6ec6d62fc5494
Ana Beatriz Martins <ana_beatriz-martins@estudante.sesisenai.org.br>
2022-03-15 19:10:00 -0300

    changing file structure

---
+++ HTML/variaveis.ts
@@ -0,0 +1,68 @@
+var cliente1: string = "Jorge"
+var cliente2 : string = "Camila"
+var cliente3: string = "Sara"
+
+var comida1: string = "abacate"
+var comida2: string = "berinjela"
+var comida3: string = "uva"
+var comida4: string = "arroz"
+var comida5: string = "maçã"
+
+var nome1: string = "Maria"
+var nome2: string = "Cristina"
+var sobrenome1: string = "Silva"
+var sobrenome2: string = "Castro"
+
+console.log(nome1,sobrenome1);
+console.log(nome2,sobrenome2);
+
+var isEntrar: boolean = true 
+var isLogar: boolean = false
+var isOnline: boolean = true
+var isAtivo: boolean = false
+var isVerdadeiro: boolean = true
+var isClicar: boolean = true
+
+var n1: number = Math.floor(Math.random()*1+1)
+var n2: number = Math.floor(Math.random()*2+1)
+var n3: number = Math.floor(Math.random()*3+1)
+var n4: number = Math.floor(Math.random()*4+1)
+var n5: number = Math.floor(Math.random()*5+1)
+var n6: number = Math.floor(Math.random()*6+1)
+var n7: number = Math.floor(Math.random()*7+1)
+var n8: number = Math.floor(Math.random()*8+1)
+
+const pi: number = 3.14; 
+const e: number = 2.71;
+const r2: number = 1.41;
+const r3: number = 1.73;
+const gn: number = 1.61; 
+
+var diferenca: number = n1-n2
+var divisao: number = n3/n1
+var Multiplique: number = n5*n2
+
+var resto: number = n4%n6
+
+function Converter() {
+    let valor = (<HTMLInputElement>document.getElementById("valor")).value;
+ alert (parseFloat (valor) *0.05+" é 5% do valor informado!")
+}
+
+var r9: number = Math.sqrt(9); 
+var ra2: number = Math.sqrt(2); 
+
+console.log(r9);
+console.log(ra2);
+
+var p5: number = Math.pow(7, 2);
+console.log(p5);
+
+var num1: number = 7;
+var num2: number = 8.5;
+var num3: number = 22;
+var num4: number = 37.8;
+
+var somaDosTotais = (num1+num2+num3+num4)
+
+console.log(somaDosTotais);
