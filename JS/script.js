460ec1cc0138162f057ceab1e3447e976aae0d0b
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-04-15 18:37:25 -0300

    Delete script.js

--- JS/script.js
+++
@@ -1,7 +0,0 @@
-let calcular = () => {
-    nota1 = document.getElementById("nota1").value
-    nota2 = document.getElementById("nota2").value
-    nota3 = document.getElementById("nota3").value
-    resultado = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) / 3;
-    console.log(resultado);
-}
