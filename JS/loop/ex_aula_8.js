334b7651298a3aa21366094a4ca02c506664244d
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-03 12:56:37 -0300

    Update ex_aula_8.js

--- JS/loop/ex_aula_8.js
+++ JS/loop/ex_aula_8.js
@@ -27,5 +27,11 @@ console.log("Média de salários: ", media.toFixed(2));
 
 // 4. Leia um número maior que 10.000 e divida ele 5 vezes.
 //Mostre ao usuário o resultado da divisão.
+numero = 15000.00
+n = 5
+for (i=0; i < n; i++){
+console.log(numero);
+numero /= 2
+}
 
 // 5. Leia 10 nomes e mostre ao usuário a quantidade de letras.
