8642bfcd8c2d3c4a2d5df1e307d4b07afcc4a7e0
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-06 11:12:41 -0300

    Update ex_aula_8.js

--- JS/loop/ex_aula_8.js
+++ JS/loop/ex_aula_8.js
@@ -24,6 +24,13 @@ for (i = 0; i < n; i++){
 }
 media = totalSalario / n
 console.log("Média de salários: ", media.toFixed(2));
+//No console:
+// I: 1 salário:  1024.71
+// I: 2 salário:  1889.99
+// I: 3 salário:  2593.05
+// I: 4 salário:  685.07
+// I: 5 salário:  1301.40
+// Média de salários:  1498.85
 
 // 4. Leia um número maior que 10.000 e divida ele 5 vezes.
 //Mostre ao usuário o resultado da divisão.
@@ -33,9 +40,26 @@ for (i=0; i < n; i++){
 console.log(numero);
 numero /= 2
 }
+//No console:
+// 15000
+// 7500
+// 3750
+// 1875
+// 937.5
 
 // 5. Leia 10 nomes e mostre ao usuário a quantidade de letras.
 let nomes = ["Jõao","Paula","Cristina","Júlia","Laura","Emilly","Caio","Bianca","Wagner","Luana"];
 for(let i = 0; i < 10; i++){
     console.log(`${nomes[i]}, ${nomes[i].length}`)
 }
+//No console:
+// Jõao, 4
+// Paula, 5
+// Cristina, 8
+// Júlia, 5
+// Laura, 5
+// Emilly, 6
+// Caio, 4
+// Bianca, 6
+// Wagner, 6
+// Luana, 5
