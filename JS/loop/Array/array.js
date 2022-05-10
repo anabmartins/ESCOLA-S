278206bac287d4609d457433444268bf3d889597
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-10 09:00:24 -0600

    Update array.js

--- JS/loop/Array/array.js
+++ JS/loop/Array/array.js
@@ -47,5 +47,9 @@ for (let i = 0; i < 3; i++){
     z = Math.floor(Math.random()*10)
     // console.log("Pontos: ", x, y, z)
     matriz.push([x, y, z])
-    console.log(matriz)
+    // console.log(matriz);
 }
+
+console.log("|", matriz[0][0], matriz[0][1], matriz[0][2] ,"|");
+console.log("|", matriz[1][0], matriz[1][1], matriz[1][2] ,"|");
+console.log("|", matriz[2][0], matriz[2][1], matriz[2][2] ,"|");
