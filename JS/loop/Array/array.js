47fa52fa1f41463a9f88c25600da13aa11ceb385
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-10 08:23:06 -0600

    Update array.js

--- JS/loop/Array/array.js
+++ JS/loop/Array/array.js
@@ -1,8 +1,12 @@
-frutas = ['uva', 'maçã', 'pera', 'laranja', 'bergamota', 'mimosa']
-for(let i = 0; i < frutas.length; i++){
-    console.log(i);
-}
+ frutas = ['uva', 'maçã', 'pera', 'laranja', 'bergamota', 'mimosa']
+ 
+ for(let i = 0; i < frutas.length; i++){
+     console.log(i);
+ }
+ for (let i = frutas.length - 1; i >= 0; i--){
+     console.log(frutas[i]);
+ }
 
-for (let i = frutas.length - 1; i >= 0; i--){
-    console.log(frutas[i]);
+for(let i = 0; i < frutas.length; i++){
+    console.log(frutas[frutas.length -1 - i])
 }
