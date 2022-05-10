bf7e1e16740b22ff2fe96b65ca72f6ef53a57ba0
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-10 08:10:05 -0600

    Add files via upload

---
+++ JS/loop/Array/array.js
@@ -0,0 +1,8 @@
+frutas = ['uva', 'maçã', 'pera', 'laranja', 'bergamota', 'mimosa']
+for(let i = 0; i < frutas.length; i++){
+    console.log(i);
+}
+
+for (let i = frutas.length - 1; i >= 0; i--){
+    console.log(frutas[i]);
+}
