01887186f45a769fb5838e6c0720c4ac57663e81
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-10 08:34:56 -0600

    Update array.js

--- JS/loop/Array/array.js
+++ JS/loop/Array/array.js
@@ -11,3 +11,14 @@ for(let i = 0; i < frutas.length; i++){
     console.log("Cálculo dos índices: " + (frutas.length-1) + " --- " + i);
     console.log(frutas[frutas.length -1 - i])
 }
+
+//expoentes em uma array
+let numeros = []
+
+let n = 3
+for (let i = 0; i < n; i++){
+    exponenciacao = 2**(n-i)
+    numeros.push(exponenciacao);
+}
+
+console.log(numeros);
