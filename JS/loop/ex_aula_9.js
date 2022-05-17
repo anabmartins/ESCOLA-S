80db251014cab08d0a58dabb17b37f83867c3fef
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-17 11:26:11 -0300

    Update ex_aula_9.js

--- JS/loop/ex_aula_9.js
+++ JS/loop/ex_aula_9.js
@@ -28,10 +28,9 @@ for (let i = 0; i < 3; i++){
     y = Math.floor(Math.random()*9)
     z = Math.floor(Math.random()*9)
     matriz.push([x, y, z])
-}
-for (let i = 0; i < 3; i++){
-        matriz[i][0][0]*=2
-        matriz[i][1][1]*=2
-        matriz[i][2][2]*=2
-        console.log("|",matriz[i][0],matriz[i][1],matriz[i][2],"|");
+    console.log("|",matriz[i][0],matriz[i][1],matriz[i][2],"|");
+    matriz[i][0][0]*=2
+    matriz[i][1][1]*=2
+    matriz[i][2][2]*=2
+    console.log("|",matriz[i][0],matriz[i][1],matriz[i][2],"|");
 }
