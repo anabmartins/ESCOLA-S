19b4646dcf43a7fe683a2e0c75c7bea8fa0ab0e9
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-17 10:48:18 -0300

    Update ex_aula_9.js

--- JS/loop/ex_aula_9.js
+++ JS/loop/ex_aula_9.js
@@ -20,4 +20,5 @@ for (let i = 0; i < n; i++){
     desconto = (valor / 100) * 5
     console.log(frutas, valor)
 }
-
+//6. Crie uma Matriz de ordem 3 e insira números aleatórios de 0 a 9. Em seguida multiplique por 2 os
+//valores da diagonal principal;
